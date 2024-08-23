f85ae2ba97b5f2cab15ebd6b6c175ff75ef1511e
VMang0 <valeriakorolkova2703@gmail.com>
2024-08-23 12:39:43 +0300

    feat: add adaptive navbar for footer and header

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { useTranslations } from 'next-intl';
+import { FC } from 'react';
+
+import { NavbarPropsType } from '@components/Navbar/types';
+import { NavLinks } from '@constants/navLinks';
+
+import style from './style.module.scss';
+
+export const Navbar: FC<NavbarPropsType> = ({ isPrivacyVisible = false }) => {
+  const trNavbar = useTranslations('Navbar');
+
+  const navLinks = isPrivacyVisible ? NavLinks : NavLinks.slice(0, NavLinks.length - 1);
+
+  return (
+    <nav>
+      <ul className={style.navbar__list}>
+        {navLinks.map(({ link, title }, index) => (
+          <li key={index}>
+            <Link href={link}>{trNavbar(title)}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
