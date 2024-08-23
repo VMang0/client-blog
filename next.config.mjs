3f8220f34708ab3308f1d103d7ed5b440c9b94a7
VMang0 <valeriakorolkova2703@gmail.com>
2024-08-23 12:23:31 +0300

    feat: base configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createNextIntlPlugin from "next-intl/plugin";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `
+      @import "@styles/breakPoints.scss";
+      @import "@styles/colors.scss";
+      @import "@styles/theme.scss";
+    `,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  },
+  images: {
+    domains: ['client-blog-server-six.vercel.app'],
+  },
+}
+
+export default withNextIntl(nextConfig)
